2c3e18fa321431c835dc4eaf6ec503c4f7188df6
Piotr <47195896+PiotrPOB@users.noreply.github.com>
2019-11-11 20:05:43 +0100

    --

--- src/app/filmy.service.ts
+++ src/app/filmy.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Film } from './modele/film';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmyService {
+
+  filmy: Film[] = [
+    { id: 0, tytul: 'Avatar', rok: 2009, opis: 'Jake, sparaliżowany były komandos, zostaje wysłany na planetę Pandora, gdzie zaprzyjaźnia się z lokalną społecznością i postanawia jej pomóc.', link: 'https://www.youtube.com/watch?v=6ziBFh3V1aM' },
+    { id: 1, tytul: 'Terminator 3', rok: 2003, opis: 'T-800 zostaje wysłany w przeszłość, by chronić Johna Connora i jego przyjaciółkę Kate Brewster. Ich przeciwnikiem jest cyborg T-X.', link: 'https://www.youtube.com/watch?v=zdYYI_2Tudg' },
+    { id: 2, tytul: 'Pulp Fiction', rok: 1994, opis: 'Przemoc i odkupienie w opowieści o dwóch płatnych mordercach pracujących na zlecenie mafii, żonie gangstera, bokserze i parze okradającej ludzi w restauracji.', link: 'https://www.youtube.com/watch?v=s7EdQ4FqbhY' },
+    { id: 3, tytul: 'Vanila Sky', rok: 2001, opis: 'Milioner zadurza się w dziewczynie przyjaciela. Tym samym ściąga na siebie gniew swojej dotychczasowej kochanki.', link: 'https://www.youtube.com/watch?v=k09OX40NLUw' }
+
+  ];
 
   constructor() { }
 }
---
+++ src/app/modele/film.ts
@@ -0,0 +1,7 @@
+export interface Film {
+    id: Number;
+    tytul: String;
+    rok?: Number; //*znak zapytania powoduje, ze pole nie jest wymagane do uzupełnienia //
+    opis?: String;
+    link?: String;
+}

d2a2a2fee355377a585e7d2ecbac7a8feb6e690a
Piotr <47195896+PiotrPOB@users.noreply.github.com>
2019-12-13 17:24:50 +0100

    Styles

--- src/app/filmy.service.ts
+++ src/app/filmy.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Film } from './modele/film';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 
 @Injectable({
@@ -9,20 +10,22 @@ import { Observable } from 'rxjs';
 })
 export class FilmyService {
 
-  private filmy: Film[] = [
-    // { id: 0, tytul: 'Avatar', rok: 2009, opis: 'Jake, sparaliżowany były komandos, zostaje wysłany na planetę Pandora, gdzie zaprzyjaźnia się z lokalną społecznością i postanawia jej pomóc.', link: 'https://www.youtube.com/watch?v=6ziBFh3V1aM' },
-    // { id: 1, tytul: 'Terminator3', rok: 2003, opis: 'T-800 zostaje wysłany w przeszłość, by chronić Johna Connora i jego przyjaciółkę Kate Brewster. Ich przeciwnikiem jest cyborg T-X.', link: 'https://www.youtube.com/watch?v=zdYYI_2Tudg' },
-    // { id: 2, tytul: 'PulpFiction', rok: 1994, opis: 'Przemoc i odkupienie w opowieści o dwóch płatnych mordercach pracujących na zlecenie mafii, żonie gangstera, bokserze i parze okradającej ludzi w restauracji.', link: 'https://www.youtube.com/watch?v=s7EdQ4FqbhY' },
-    // { id: 3, tytul: 'VanillaSky', rok: 2001, opis: 'Milioner zadurza się w dziewczynie przyjaciela. Tym samym ściąga na siebie gniew swojej dotychczasowej kochanki.', link: 'https://www.youtube.com/watch?v=k09OX40NLUw' }
-  ];
+  // private filmy: Film[] = [
+  // { id: 0, tytul: 'Avatar', rok: 2009, opis: 'Jake, sparaliżowany były komandos, zostaje wysłany na planetę Pandora, gdzie zaprzyjaźnia się z lokalną społecznością i postanawia jej pomóc.', link: 'https://www.youtube.com/watch?v=6ziBFh3V1aM' },
+  // { id: 1, tytul: 'Terminator3', rok: 2003, opis: 'T-800 zostaje wysłany w przeszłość, by chronić Johna Connora i jego przyjaciółkę Kate Brewster. Ich przeciwnikiem jest cyborg T-X.', link: 'https://www.youtube.com/watch?v=zdYYI_2Tudg' },
+  // { id: 2, tytul: 'PulpFiction', rok: 1994, opis: 'Przemoc i odkupienie w opowieści o dwóch płatnych mordercach pracujących na zlecenie mafii, żonie gangstera, bokserze i parze okradającej ludzi w restauracji.', link: 'https://www.youtube.com/watch?v=s7EdQ4FqbhY' },
+  // { id: 3, tytul: 'VanillaSky', rok: 2001, opis: 'Milioner zadurza się w dziewczynie przyjaciela. Tym samym ściąga na siebie gniew swojej dotychczasowej kochanki.', link: 'https://www.youtube.com/watch?v=k09OX40NLUw' }
+  // ];
+
+  private baseURL: String = environment.apiURL;
 
   constructor(private http: HttpClient) { }
 
   wszystkieFilmy(): Observable<any> {
-    return this.http.get('http://127.0.0.1:8000/apifilmy/');
+    return this.http.get(`${this.baseURL}/apifilmy/`);
   }
 
   getFilm(id: number): Observable<Film> {
-    return this.http.get<Film>(`http://127.0.0.1:8000/apifilmy/${id}/`);
+    return this.http.get<Film>(`${this.baseURL}/apifilmy/${id}/`);
   }
 }
